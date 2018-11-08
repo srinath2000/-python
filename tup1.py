2ca1180e4195e91d1e11990668a40dda19481168
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2018-11-08 13:52:17 +0530

    Update tup1.py

--- tup1.py
+++ tup1.py
@@ -1,5 +1,4 @@
-#salary=(10000,71000,6589,452638,92392,2346,1000,63738,64333,8999)
-salary=(1,2,3)
+salary=(10000,71000,6589,452638,92392,2346,1000,63738,64333,8999)
 print salary
 print max(salary)
 print min(salary)
@@ -8,10 +7,3 @@ for i in salary:
 	s=(s+i)/(len(salary))
 print s
 
-'''def area(r):
-	areac=3.14*r*r
-	circum=2*3.14*r
-	return (areac,circum)
-
-(x,y)=area(1)
-print x,y'''
